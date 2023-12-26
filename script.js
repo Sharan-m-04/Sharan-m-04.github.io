89c9f9bac0c4222b59c8ee6922daac49499eccc7
Sharan M <msharan.hnp@gmail.com>
2023-12-26 21:43:18 +0530

    added scroll animation

--- script.js
+++ script.js
@@ -163,3 +163,41 @@ function SkeezyProject() {
     window.open(
         "https://www.figma.com/file/GalviVgIyYuSJoSV6ezBDs/Hackathon-22", "_blank");
 }
+
+//Project: Scroll Animation
+
+function reveal_left() {
+    var reveals = document.querySelectorAll(".reveal-left");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal_left);
+
+function reveal_right() {
+    var reveals = document.querySelectorAll(".reveal-right");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal_right);
