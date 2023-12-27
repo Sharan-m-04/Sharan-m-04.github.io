9aa8ecd13172588ff189ba33a4417c0d33c5a28e
Sharan M <msharan.hnp@gmail.com>
2023-12-27 21:42:16 +0530

    adding skills section

--- script.js
+++ script.js
@@ -201,3 +201,49 @@ function reveal_right() {
 }
 
 window.addEventListener("scroll", reveal_right);
+
+//Skills: Category Filter
+
+filterSelection("all")
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("skill-card");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+        RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+    }
+}
+
+function AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+    }
+}
+
+function RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+var btnContainer = document.getElementById("skill-chips-div");
+var btns = btnContainer.getElementsByClassName("skill-chip");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function (event) {
+        var current = btnContainer.getElementsByClassName("active");
+        for (var j = 0; j < current.length; j++) {
+            current[j].classList.remove("active");
+        }
+        event.target.classList.add("active");
+    });
+}
