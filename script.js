0b5df4a743b1ba33555b0aadb8902d0821ec1d41
Sharan M <msharan.hnp@gmail.com>
2023-12-31 16:05:18 +0530

    code improvements

--- script.js
+++ script.js
@@ -111,27 +111,6 @@ function contactBtn() {
     window.location.href = "#contact-me";
 }
 
-//About: Scroll Animation
-
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
-    }
-}
-
-window.addEventListener("scroll", reveal);
-
-
 //Project: Open Projects
 
 function portfolioProject() {
@@ -163,44 +142,6 @@ function SkeezyProject() {
     window.open(
         "https://www.figma.com/file/GalviVgIyYuSJoSV6ezBDs/Hackathon-22", "_blank");
 }
-
-//Project: Scroll Animation
-
-function reveal_left() {
-    var reveals = document.querySelectorAll(".reveal-left");
-
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
-    }
-}
-
-window.addEventListener("scroll", reveal_left);
-
-function reveal_right() {
-    var reveals = document.querySelectorAll(".reveal-right");
-
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
-    }
-}
-
-window.addEventListener("scroll", reveal_right);
 
 //Skills: Category Filter
 
@@ -247,26 +188,6 @@ for (var i = 0; i < btns.length; i++) {
         event.target.classList.add("active");
     });
 }
-
-//Skills: Scroll Animation
-
-function normal_fade() {
-    var reveals = document.querySelectorAll(".fade");
-
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
-    }
-}
-
-window.addEventListener("scroll", normal_fade);
 
 //Awards: Opening image in new tab
 
@@ -380,3 +301,88 @@ function UI_UX_HackathonCertificate() {
         </html>
     `);
 }
+
+// Animations
+
+
+// Pop Scroll Animation
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+// Fade left Scroll Animation
+
+function reveal_left() {
+    var reveals = document.querySelectorAll(".reveal-left");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal_left);
+
+// Fade Right Scroll Animation
+
+function reveal_right() {
+    var reveals = document.querySelectorAll(".reveal-right");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal_right);
+
+
+// Normal Fade Scroll Animation
+
+function normal_fade() {
+    var reveals = document.querySelectorAll(".fade");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", normal_fade);
