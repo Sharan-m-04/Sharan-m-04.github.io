2aab2e2ca7f50ff74653e6e8efe924f165796a8c
Sharan M <msharan.hnp@gmail.com>
2024-09-02 23:01:58 +0530

    download resume not found fixed

--- script.js
+++ script.js
@@ -126,7 +126,7 @@ window.onload = function () {
 //Home: Download Button
 
 function resumeBtn() {
-    window.location.href = "/Downloads/Sharan M Resume.pdf";
+    window.location.href = "/Downloads/Sharan'sResume.pdf";
 }
 
 //About: Contact Button
